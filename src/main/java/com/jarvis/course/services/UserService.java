1e7e52ff448fb7eea843833003aaf8ba32b202cb
sillasconde <sillasconde@usp.br>
2024-06-30 20:29:48 -0300

    Exception handling - update

--- src/main/java/com/jarvis/course/services/UserService.java
+++ src/main/java/com/jarvis/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.jarvis.course.repositories.UserRepository;
 import com.jarvis.course.services.exceptions.DatabaseException;
 import com.jarvis.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -40,6 +42,7 @@ public class UserService {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
+			// JPA is not throwing the exception.
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
@@ -47,9 +50,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
