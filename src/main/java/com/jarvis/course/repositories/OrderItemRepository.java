aaadd626fbf587e2745da2e083cc711de335f401
sillasconde <sillasconde@usp.br>
2024-06-30 13:49:39 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/jarvis/course/repositories/OrderItemRepository.java
+++ src/main/java/com/jarvis/course/repositories/OrderItemRepository.java
@@ -3,9 +3,10 @@ package com.jarvis.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jarvis.course.entities.OrderItem;
+import com.jarvis.course.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 	
 	
